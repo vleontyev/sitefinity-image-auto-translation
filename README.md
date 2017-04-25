fc2523f6d5c42952996bd328135384210a10517a
Victor Leontyev <victor.leontev@gmail.com>
2017-04-25 23:52:28 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# sitefinity-image-auto-translation
+# Image auto translation for Sitefinity
+
+When you upload image and publish it, it will automatically create another translations for this image
+
+## Install
+
+1.  Copy file /src/AutoImageTranslator.cs into your solution
+2.  In your Global.asax.cs file add this code:
+
+```
+protected void Application_Start(object sender, EventArgs e)
+{
+    Telerik.Sitefinity.Abstractions.Bootstrapper.Initialized += Bootstrapper_Initialized;
+}
+protected void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs args)
+{
+    if (args.CommandName == "Bootstrapped")
+    {
+        EventHub.Subscribe<IDataEvent>(AutoImageTranslator.Action);
+    }
+}
+```
---
+++ src/AutoImageTranslator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Data.Events;
+using Telerik.Sitefinity.GenericContent.Model;
+using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Services;
+
+namespace SitefinityWebApp
+{
+    public class AutoImageTranslator
+    {
+        public static void Action(IDataEvent e)
+        {
+            var action = e.Action;
+            var contentType = e.ItemType;
+            if (action == "New" && contentType.FullName == "Telerik.Sitefinity.Libraries.Model.Image")
+            {
+                var languages = SystemManager.CurrentContext.AppSettings.DefinedFrontendLanguages;
+                if (languages.Length > 1)
+                {
+                    var culture = Thread.CurrentThread.CurrentUICulture;
+                    var itemId = e.ItemId;
+                    var providerName = e.ProviderName;
+                    var librariesManager = LibrariesManager.GetManager(providerName);
+                    var image = librariesManager.GetImage(itemId);
+                    if (image.Status == ContentLifecycleStatus.Master)
+                    {
+                        var title = image.Title;
+                        foreach (var language in languages)
+                        {
+                            if (!image.AvailableCultures.Any(i => i.Name == language.Name))
+                            {
+                                Thread.CurrentThread.CurrentCulture = language;
+                                Thread.CurrentThread.CurrentUICulture = language;
+
+                                image.Title = title;
+                                librariesManager.SaveChanges();
+                                librariesManager.Lifecycle.Publish(image);
+                                librariesManager.SaveChanges();
+                            }
+                        }
+                        Thread.CurrentThread.CurrentCulture = culture;
+                        Thread.CurrentThread.CurrentUICulture = culture;
+                    }
+                }
+            }
+        }
+    }
+}
